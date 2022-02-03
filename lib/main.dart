a0a20c0a5e590dbec888aba598dd033b3e2b8073
hernandez87v <hernandez87v@gmail.com>
2022-02-03 15:43:39 -0800

    add TextEditingController to ChatScreen

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,17 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
