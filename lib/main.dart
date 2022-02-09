b5ac8f65ed6dba5505cca12623a3464e36ea2e37
hernandez87v <hernandez87v@gmail.com>
2022-02-09 10:24:48 -0800

    add SizeTransition widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,33 +37,38 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child: Text(_name[0])),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                _name,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                width: 412,
-                child: Text(
-                  text,
-                  overflow: TextOverflow.visible,
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  _name,
+                  style: Theme.of(context).textTheme.headline4,
                 ),
-              ),
-            ],
-          ),
-        ],
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  width: 412,
+                  child: Text(
+                    text,
+                    overflow: TextOverflow.visible,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
